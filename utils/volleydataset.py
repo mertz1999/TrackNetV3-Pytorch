de2f5139b5ca5a80ce2d42181c06b23d17f11186
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-07-07 19:57:47 +0430

    (ADD) add heatmap to dataset

--- utils/volleydataset.py
+++ utils/volleydataset.py
@@ -3,8 +3,8 @@ import cv2
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import torch
 from torchvision import transforms
+from generate_hearmap import genHeatMap
 from torch.utils.data import Dataset, DataLoader
 
 
@@ -19,11 +19,13 @@ class VollyDataset(Dataset):
 
     """
     # Define initial function
-    def __init__(self, dataset_path, width=512, height=288):
+    def __init__(self, dataset_path, r=3, mag=1, width=512, height=288):
         print(" ---------- Dataset in loaded ---------- \n")
         self.dataset = pd.read_csv(dataset_path)
         self.width   = width
         self.height  = height
+        self.r       = r
+        self.mag     = mag
 
         # Define Transform list 
         # --- Define Transforms
@@ -31,6 +33,11 @@ class VollyDataset(Dataset):
                                     transforms.ToPILImage(),
                                     transforms.ToTensor(),
                                     transforms.Normalize(mean=[0,0,0],std =[1,1,1])
+                                ])
+        
+        self.transform_label = transforms.Compose([
+                                    transforms.ToPILImage(),
+                                    transforms.ToTensor()
                                 ])
 
     # getitem function
@@ -40,6 +47,8 @@ class VollyDataset(Dataset):
 
         # read video
         cap = cv2.VideoCapture(selected_row['video_path'])
+        vid_width  = cap. get(cv2. CAP_PROP_FRAME_WIDTH )
+        vid_height = cap. get(cv2. CAP_PROP_FRAME_HEIGHT)
 
         # First image
         cap.set(cv2.CAP_PROP_POS_FRAMES, selected_row['frame_idx'])
@@ -55,13 +64,37 @@ class VollyDataset(Dataset):
 
         # Preprocess path
         image_1, image_2, image_3 = map(self.base_transform, [image_1, image_2, image_3])
+
+        # Label arrays
+        label_1 = genHeatMap(self.width, 
+                             self.height, 
+                             (selected_row['cord_1_x']*self.width)//vid_width,
+                             (selected_row['cord_1_y']*self.height)//vid_height,self.r,self.mag
+                             )
+        label_2 = genHeatMap(self.width, 
+                             self.height, 
+                             (selected_row['cord_2_x']*self.width)//vid_width,
+                             (selected_row['cord_2_y']*self.height)//vid_height,self.r,self.mag
+                             )
+        label_3 = genHeatMap(self.width, 
+                             self.height, 
+                             (selected_row['cord_3_x']*self.width)//vid_width,
+                             (selected_row['cord_3_y']*self.height)//vid_height,self.r,self.mag
+                             )
         
-        # Apply transform
-        output        = np.zeros((self.height, self.width, 3), dtype=np.uint8)
-        output[:,:,0] = image_1; output[:,:,1] = image_2; output[:,:,2] = image_3
-        output        = self.transform(output)
         
-        return output, selected_row['video_path']
+        # Apply transform to inputs images
+        input_images        = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        input_images[:,:,0] = image_1; input_images[:,:,1] = image_2; input_images[:,:,2] = image_3
+        input_images        = self.transform(input_images)
+
+        # Apply transform to label images
+        label_images        = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        label_images[:,:,0] = label_1; label_images[:,:,1] = label_2; label_images[:,:,2] = label_3
+        label_images        = self.transform_label(label_images)
+
+        
+        return input_images, label_images
 
 
     # return len of triplets
@@ -79,7 +112,7 @@ class VollyDataset(Dataset):
 
 
 ######### --------------- TEST --------------- #########
-# volley_dataloader = DataLoader(VollyDataset('merged_dataset.csv', 1080, 720), 
+# volley_dataloader = DataLoader(VollyDataset('merged_dataset.csv'), 
 #                        batch_size= 5,
 #                        shuffle=True,
 #                     #    num_workers=1,
@@ -88,12 +121,17 @@ class VollyDataset(Dataset):
 
 
 # for i,j in volley_dataloader:
-#     img = i[0].numpy()
-#     for i in range(3):
-#         X = img[i]
-#         plt.imshow(X)
+#     img   = i[0].numpy()
+#     label = j[0].numpy()
+#     for k in range(3):
+#         X = img[k]
+#         Y = label[k]
+#         plt.subplot(1,2, 1)
+#         plt.imshow(X+Y*255)
+#         plt.subplot(1,2, 2)
+#         plt.imshow(Y)
 #         plt.show()
-#     print(img[2].shape);exit()
+    # print(i.shape, j.shape);exit()
 
 # X = next(iter(volley_dataloader))
 # print(X.shape)
