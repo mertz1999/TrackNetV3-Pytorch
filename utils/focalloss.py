5c8889364f411f33728c1f24504c23cc04fb56dc
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-07-09 21:31:48 +0430

    (FIX) prev. focallost i think didnot work correctly si i decided to chagee it

--- utils/focalloss.py
+++ utils/focalloss.py
@@ -3,6 +3,30 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class FocalLoss2(nn.Module):
+    def __init__(self, alpha=1, gamma=2, reduction: str = 'mean'):
+        super().__init__()
+        if reduction not in ['mean', 'none', 'sum']:
+            raise NotImplementedError('Reduction {} not implemented.'.format(reduction))
+        self.reduction = reduction
+        self.alpha = alpha
+        self.gamma = gamma
+
+    def forward(self, x, target):
+        eps = np.finfo(float).eps
+        p_t = torch.where(target == 1, x, 1-x)
+        fl = - 1 * (1 - p_t) ** self.gamma * torch.log(p_t + eps)
+        fl = torch.where(target == 1, fl * self.alpha, fl * (1 - self.alpha))
+        return self._reduce(fl)
+
+    def _reduce(self, x):
+        if self.reduction == 'mean':
+            return x.mean()
+        elif self.reduction == 'sum':
+            return x.sum()
+        else:
+            return x
+
 
 
 class FocalLoss(nn.Module):
@@ -22,24 +46,36 @@ class FocalLoss(nn.Module):
 
         # Find distance to gound truth
         pt = np.array(list(map(lambda p,y: p.item() if y==1 else 1-p.item(), output, targets)))
+        pt = np.where(pt == 0, 1e-7, pt)
 
         # make log
         log = -1*np.log(pt)
-        log = np.where(log == np.inf, 1e-7, log)
 
         # add gamma and alpha
         focal_loss = ((1-pt)**self.gamma) * log
 
         if self.alpha != None:
-            focal_loss = list(map(lambda x, y: x*self.alpha if y==1 else x , focal_loss, targets))
+            focal_loss = list(map(lambda x, y: x*self.alpha if y==1 else x*(1-self.alpha) , focal_loss, targets))
 
         # return focal_loss
-        return torch.tensor(np.mean(focal_loss))
+        return torch.mean(torch.tensor(focal_loss, requires_grad=True))
 
 
 
-# x = torch.from_numpy(np.array([0.1, 0.6, 0.9]))
-# y = torch.from_numpy(np.array([0, 1, 1]))
+# x = torch.tensor([0.1, 0.6, 0.9], requires_grad=True)
+# y = torch.tensor([0., 1., 1.], requires_grad=False)
 
 # loss = FocalLoss(alpha=3)
-# print(loss.forward(x, y))
+# loss_value = loss.forward(x, y)
+# test_loss = nn.BCELoss()
+# loss_value = test_loss.forward(x, y)
+# print(x.grad, y.grad)
+# loss_value.backward()
+# print(x.grad, y.grad)
+
+
+# temp = nn.Softmax()
+# X_temp = temp(x)
+# print(X_temp.requires_grad)
+# Y_temp = temp(y)
+# print(Y_temp.requires_grad)
