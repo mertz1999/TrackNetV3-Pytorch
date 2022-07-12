5b19983fe3c8551e86cc8b12d7d4e8f218300a44
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-07-12 23:11:45 +0430

    (FEAT) Motion channel creator for better result

---
+++ utils/motion_channel.py
@@ -0,0 +1,16 @@
+import cv2 
+import numpy as np
+
+def motion_channel(img1, img2, img3):
+    hsv_fut  = cv2.cvtColor(img3, cv2.COLOR_BGR2HSV)
+    hsv_now  = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
+    hsv_pre  = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)
+
+    hsv_fut = hsv_fut.astype(np.float64)
+    hsv_now = hsv_now.astype(np.float64)
+    hsv_pre = hsv_pre.astype(np.float64)
+
+    tot_diff  = (hsv_pre[:,:,2]- hsv_now[:,:,2]) + (hsv_fut[:,:,2] - hsv_now[:,:,2])
+    tot_diff  = (tot_diff - np.min(tot_diff))/(np.max(tot_diff) - np.min(tot_diff))
+
+    return tot_diff
