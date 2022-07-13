4e011e264665b2a083021123f82839cd0f678dfd
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-07-13 15:35:56 +0430

    (REFACTOR) Write log file on previos

--- utils/utils.py
+++ utils/utils.py
@@ -2,10 +2,10 @@
 
 
 class Print():
-    def __init__(self, log_path='log.txt'):
+    def __init__(self, log_path='log.txt', append=True):
         # Output log file
         self.log_path = log_path
-        self.output_log = open(log_path, 'w')
+        self.output_log = open(log_path, 'a' if append==True else'w')
         self.output_log.close()
     
     def __call__(self, *args):
