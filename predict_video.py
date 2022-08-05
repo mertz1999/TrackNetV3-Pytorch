b26960a43bcb48c4c1673f583fb647c1284133c0
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-08-06 01:00:35 +0430

    (NOT IMPORTANT)

--- predict_video.py
+++ predict_video.py
@@ -104,85 +104,89 @@ while(frame_idx <= total_frame-2):
     print(f'Reading Frame {frame_idx}')
 
     # Third image channel
-    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx+2)
-    _, image_3 = cap.read()
-    image3_cp = np.copy(image_3)
+    try:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx+2)
+        _, image_3 = cap.read()
+        image3_cp = np.copy(image_3)
 
-    # Secound image channel
-    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx+1)
-    _, image_2 = cap.read()
-    image2_cp = np.copy(image_2)
+        # Secound image channel
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx+1)
+        _, image_2 = cap.read()
+        image2_cp = np.copy(image_2)
 
-    # First image channel
-    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
-    _, image_1 = cap.read()
-    image1_cp = np.copy(image_1)
+        # First image channel
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
+        _, image_1 = cap.read()
+        image1_cp = np.copy(image_1)
 
-    input_images_list = [image1_cp, image2_cp, image3_cp]
+        input_images_list = [image1_cp, image2_cp, image3_cp]
 
-    # Fourth image channel (Motion channel)
-    # Preprocess path
-    image_4 = motion_channelV3(image_2, image_3)
-    image_4 = cv2.resize(image_4, (WIDTH, HEIGHT))
+        # Fourth image channel (Motion channel)
+        # Preprocess path
+        image_4 = motion_channelV3(image_2, image_3)
+        image_4 = cv2.resize(image_4, (WIDTH, HEIGHT))
 
-    image_5 = motion_channelV3(image_1, image_2)
-    image_5 = cv2.resize(image_5, (WIDTH, HEIGHT))
+        image_5 = motion_channelV3(image_1, image_2)
+        image_5 = cv2.resize(image_5, (WIDTH, HEIGHT))
 
-    # Preprocesss images
-    image_1, image_2, image_3 = map(base_transform, [image_1, image_2, image_3])
+        # Preprocesss images
+        image_1, image_2, image_3 = map(base_transform, [image_1, image_2, image_3])
 
-    # Apply transform to inputs images 
-    input_images        = np.zeros((HEIGHT, WIDTH, 5), dtype=np.uint8)
-    input_images[:,:,0] = image_1; input_images[:,:,1] = image_2; input_images[:,:,2] = image_5; input_images[:,:,3] = image_3; input_images[:,:,4] = image_4
-    input_images        = transform(input_images).unsqueeze(0)
-
-
-    
-
-    # Predict this frames
-    input_images = input_images.to(device)
-    pred_images  = model(input_images)
+        # Apply transform to inputs images 
+        input_images        = np.zeros((HEIGHT, WIDTH, 5), dtype=np.uint8)
+        input_images[:,:,0] = image_1; input_images[:,:,1] = image_2; input_images[:,:,2] = image_5; input_images[:,:,3] = image_3; input_images[:,:,4] = image_4
+        input_images        = transform(input_images).unsqueeze(0)
 
 
-    # Postproccess
-    pred_images = pred_images.cpu().detach()
-    pred_images = pred_images.squeeze(0)
-    pred_images = pred_images.numpy()
-    out_image   = pred_images.copy()
-    pred_images = pred_images > 0.5
-    pred_images = pred_images * 255
-    pred_images = pred_images.astype(np.uint8)
-
-    pred_images_copy = pred_images.copy()
-
-
-    for channel in range(3):
-        X = pred_images_copy[channel]
-        (cnts, _) = cv2.findContours(X, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        rects = [cv2.boundingRect(ctr) for ctr in cnts]
-        max_area_idx = 0
-        try:
-            max_area = rects[max_area_idx][2] * rects[max_area_idx][3]
-            for i in range(len(rects)):
-                area = rects[i][2] * rects[i][3]
-                if area > max_area:
-                    max_area_idx = i
-                    max_area = area
-            target = rects[max_area_idx]
-
-            (cx_pred, cy_pred) = (
-                int((target[0] + target[2] / 2) * (width/WIDTH)),
-                int((target[1] + target[3] / 2) * (height/HEIGHT)),
-                )
-        except:
-            (cx_pred, cy_pred) = (0,0)
         
-        cv2.circle(input_images_list[channel], (cx_pred, cy_pred), 5, (0,0,255), -1)
-        f.write(f'{frame_idx+channel},{0 if (cx_pred, cy_pred) == (0,0) else 1},{cx_pred},{cy_pred}\n')
-        
-        # plt.imshow(input_images_list[channel]);plt.show()
 
-        # out_vid.write(input_images_list[channel])
+        # Predict this frames
+        input_images = input_images.to(device)
+        pred_images  = model(input_images)
+
+
+        # Postproccess
+        pred_images = pred_images.cpu().detach()
+        pred_images = pred_images.squeeze(0)
+        pred_images = pred_images.numpy()
+        out_image   = pred_images.copy()
+        pred_images = pred_images > 0.5
+        pred_images = pred_images * 255
+        pred_images = pred_images.astype(np.uint8)
+
+        pred_images_copy = pred_images.copy()
+
+
+        for channel in range(3):
+            X = pred_images_copy[channel]
+            (cnts, _) = cv2.findContours(X, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            rects = [cv2.boundingRect(ctr) for ctr in cnts]
+            max_area_idx = 0
+            try:
+                max_area = rects[max_area_idx][2] * rects[max_area_idx][3]
+                for i in range(len(rects)):
+                    area = rects[i][2] * rects[i][3]
+                    if area > max_area:
+                        max_area_idx = i
+                        max_area = area
+                target = rects[max_area_idx]
+
+                (cx_pred, cy_pred) = (
+                    int((target[0] + target[2] / 2) * (width/WIDTH)),
+                    int((target[1] + target[3] / 2) * (height/HEIGHT)),
+                    )
+            except:
+                (cx_pred, cy_pred) = (0,0)
+            
+            cv2.circle(input_images_list[channel], (cx_pred, cy_pred), 5, (0,0,255), -1)
+            f.write(f'{frame_idx+channel},{0 if (cx_pred, cy_pred) == (0,0) else 1},{cx_pred},{cy_pred}\n')
+            
+            # plt.imshow(input_images_list[channel]);plt.show()
+
+            out_vid.write(input_images_list[channel])
+    except:
+        break
+
 
     frame_idx += 3
 
